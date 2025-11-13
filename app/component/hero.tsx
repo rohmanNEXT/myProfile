94f937c0067dc280fc81be3100b19dedd8e47524
man <111178009+rohmanNEXT@users.noreply.github.com>
2025-11-13 10:25:38 +0800

    Update hero.tsx

--- app/component/hero.tsx
+++ app/component/hero.tsx
@@ -28,7 +28,7 @@ const Hero: React.FC = () => {
 
           {/* Tombol Contact WhatsApp */}
   <a
-  href="https://wa.me/085646831030"
+  href="https://wa.me/6285646831030"
   target="_blank"
   rel="noopener noreferrer"
   className="flex items-center justify-between gap-3 
