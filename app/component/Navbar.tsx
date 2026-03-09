5172147dadf4bd49f8275a8c61a02229c7882075
Rohman . <bluekraken@gmail.com>
2026-03-09 11:37:30 +0800

    feat: hallo

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -32,23 +32,23 @@ export default function Navbar() {
           {/* Logo */}
           <div
             onClick={goHome}
-            className="text-2xl sm:text-3xl font-medium cursor-pointer tracking-tight px-4"
+            className="text-2xl sm:text-3xl font-medium cursor-pointer tracking-tight px-4 cursor-pointer"
           >
             Rohman<span className="text-[#f9d38e]">.dev</span>
           </div>
 
           {/* Menu Desktop */}
           <div className="hidden md:flex items-center gap-10 text-lg font-light px-6">
-            <button onClick={() => goId("Home")} className="hover:text-blue-400 transition">
+            <button onClick={() => goId("Home")} className="hover:text-blue-400 transition cursor-pointer">
               Home
             </button>
-            <button onClick={() => goId("Project")} className="hover:text-blue-400 transition">
+            <button onClick={() => goId("Project")} className="hover:text-blue-400 transition cursor-pointer">
               Project 
             </button>
-            <button onClick={() => goId("YouTube")} className="hover:text-blue-400 transition items-center flex">
+            <button onClick={() => goId("YouTube")} className="hover:text-blue-400 transition items-center flex cursor-pointer">
               YouTube <MdArrowOutward className="ml-2"/>
             </button>
-            <button onClick={() => goId("Contact")} className="hover:text-blue-400 transition">
+            <button onClick={() => goId("Contact")} className="hover:text-blue-400 transition cursor-pointer">
               Kontak
             </button>
           </div>
@@ -59,7 +59,7 @@ export default function Navbar() {
               onClick={linkldn}
               color="light"
               variant="outline"
-              className="text-[#e1f1fd] border border-[#e1f1fd] rounded-full px-4 py-1 hover:bg-[#e1f1fd] hover:text-[#0a2e47] transition"
+              className="text-[#e1f1fd] border border-[#e1f1fd] rounded-full px-4 py-1 hover:bg-[#e1f1fd] hover:text-[#0a2e47] transition cursor-pointer"
             >
               linkeldn 
             </CButton>
@@ -77,29 +77,29 @@ export default function Navbar() {
 
       {/* ✅ MENU MOBILE DIPINDAH KE LUAR NAVBAR */}
       {open && (
-     <div className="fixed top-20 left-1/2 -translate-x-1/2 z-40 md:hidden flex flex-col items-center w-[90%] bg-[#1e1b4b]/95 backdrop-blur-md border border-[#e1f1fd]/20 rounded-2xl shadow-xl py-6 space-y-3 text-base animate-slideDown">
+     <div className="fixed top-20 left-1/2 -translate-x-1/2 z-40 md:hidden flex flex-col items-center w-[90%] bg-[#1e1b4b]/95 backdrop-blur-md border border-[#e1f1fd]/20 rounded-2xl shadow-xl py-6 space-y-3 text-base animate-slideDown cursor-pointer">
 
           <button
             onClick={() => goId("Home")}
-            className="w-full text-center py-2 text-[#e0f2fe] hover:bg-[#38bdf8]/20 hover:text-[#bae6fd] transition rounded-lg"
+            className="w-full text-center py-2 text-[#e0f2fe] hover:bg-[#38bdf8]/20 hover:text-[#bae6fd] transition rounded-lg cursor-pointer"
           >
             Home
           </button>
           <button
             onClick={() => goId("Project")}
-            className="w-full text-center py-2 text-[#e0f2fe] hover:bg-[#38bdf8]/20 hover:text-[#bae6fd] transition rounded-lg"
+            className="w-full text-center py-2 text-[#e0f2fe] hover:bg-[#38bdf8]/20 hover:text-[#bae6fd] transition rounded-lg cursor-pointer"
           >
            Project
           </button>
           <button
             onClick={() => goId("YouTube")}
-            className="w-full text-center py-2 text-[#e0f2fe] hover:bg-[#38bdf8]/20 hover:text-[#bae6fd] transition rounded-lg flex items-center justify-center"
+            className="w-full text-center py-2 text-[#e0f2fe] hover:bg-[#38bdf8]/20 hover:text-[#bae6fd] transition rounded-lg flex items-center justify-center cursor-pointer"
           >
             YouTube <MdArrowOutward className="ml-2"/>
           </button>
           <button
             onClick={() => goId("Contact")}
-            className="w-full text-center py-2 text-[#e0f2fe] hover:bg-[#38bdf8]/20 hover:text-[#bae6fd] transition rounded-lg"
+            className="w-full text-center py-2 text-[#e0f2fe] hover:bg-[#38bdf8]/20 hover:text-[#bae6fd] transition rounded-lg cursor-pointer"
           >
             Kontak
           </button>
@@ -109,7 +109,7 @@ export default function Navbar() {
             color="light"
             variant="outline"
             className="text-[#e0f2fe] border border-[#7dd3fc]/40 rounded-full px-5 py-2 
-              hover:bg-[#7dd3fc] hover:text-[#0f172a] transition"
+              hover:bg-[#7dd3fc] hover:text-[#0f172a] transition cursor-pointer"
           >
             linkldn 
           </CButton>
